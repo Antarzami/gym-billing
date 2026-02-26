25738a06363a9ce0da46f958f6668db5a6c10a9d
Mahima Alekha <abhijeetswain18075@gmail.com>
2026-02-26 20:08:54 +0530

    Added permanent admin login

--- src/main/java/com/abhijit/gymbilling/config/SecurityConfig.java
+++ src/main/java/com/abhijit/gymbilling/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +15,19 @@ import org.springframework.security.authentication.AuthenticationManager;
 
 @Configuration
 public class SecurityConfig {
+
+    // ✅ Permanent Admin Login
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin123"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
